01c56279bdc977c12248c31a1bd11268d51d8499
pokachtononame <q880534@gmail.com>
2026-02-26 04:45:27 +0300

    Fixed PEP-8 warnings

--- src/individual_2.py
+++ src/individual_2.py
@@ -18,35 +18,38 @@ if __name__ == "__main__":
             grades_list = input("Успеваемость (5 оценок через пробел): ").split()
 
             grades = []
-            error = False
+            has_error = False
             for i in range(5):
                 try:
                     grade = int(grades_list[i])
                     if grade < 1 or grade > 5:
                         print("\nОшибка: неправильный формат данных", file=sys.stderr)
-                        error = True
+                        has_error = True
                         break
                     grades.append(grade)
                 except ValueError:
                     print("\nОшибка: неправильный тип данных", file=sys.stderr)
-                    error = True
+                    has_error = True
                     break
 
             if len(grades) < 5:
                 print("\nОшибка: неправильное количество оценок", file=sys.stderr)
-                error = True
+                has_error = True
 
             student = {
                 'name': name,
                 'group': group,
                 'grades': grades
             }
-            if not error:
+            if not has_error:
                 students.append(student)
 
         elif command == 'list':
             if len(students) > 1:
-                students.sort(key=lambda item: sum(item.get('grades')) / len(item.get('grades')), reverse=True)
+                students.sort(
+                    key=lambda item: sum(item.get('grades')) / len(item.get('grades')),
+                    reverse=True
+                )
 
             line = '+-{}-+-{}-+-{}-+'.format(
                 '-' * 20,
@@ -78,18 +81,17 @@ if __name__ == "__main__":
             count = 0
             for student in students:
                 grades = student.get('grades', [])
-                if 4 in grades or 5 in grades:
+                if 4 in grades and 5 in grades:
                     count += 1
                     print(
                         '{:>4}: {}'.format(count, student.get('name', ''))
                     )
 
-
         elif command == 'help':
             print("\nСписок команд:")
             print("add - добавить студента")
             print("list - вывести список студентов")
-            print("select - запросить студентов с оценкой 4 или 5")
+            print("select - запросить студентов с оценкой 4 и 5")
             print("help - отобразить справку")
             print("exit - завершить работу с программой\n")
         else:
